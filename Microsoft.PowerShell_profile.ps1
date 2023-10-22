73a6ec1e4fdcd28e1b8c1ef79e04c745fa5ed214
Tema <>
2023-10-22 16:03:32 +0200

    Init

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,49 @@
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+$env:SHELL = '/usr/bin/pwsh'
+$env:EDITOR = 'mcedit'
+$env:PATH += @(
+    '',
+    '~/.local/bin/',
+    '~/bin',
+    '/home/linuxbrew/.linuxbrew/bin',
+    '/.cargo/bin',
+    $(trdl bin-path werf 1.2 stable)
+) | Join-String -Separator ":"
+
+oh-my-posh --init --shell pwsh --config ~/.poshthemes/kkthxbye.omp.json | Invoke-Expression
+
+# helm completion powershell | Out-String | Invoke-Expression
+# Import-Module posh-dotnet
+# Import-Module DockerCompletion
+# Import-Module posh-git
+# Import-Module PSKubectlCompletion
+# Import-Module npm-completion
+
+Import-Module powershell-yaml
+
+function snowsqlps {
+    snowsql --config ~/.snowsql/csv $args | Out-String | ConvertFrom-Csv
+}
+
+function PsqlToCsv {
+    param (
+        [Parameter(Mandatory)]
+        [string]$Query
+    )
+    return "copy ($($Query.TrimEnd(";", "`n"))) to stdout with csv header delimiter ',';"
+}
+
+function psqlps {
+    param (
+        [Parameter(Mandatory, Position=0)]
+        [string]$ConnectionAlias,
+
+        [Parameter(Mandatory, Position=1)]
+        [string]$Query
+    )
+    $env:PGSERVICE = $ConnectionAlias
+    psql --command "$(PsqlToCsv($Query))" | ConvertFrom-Csv
+}
