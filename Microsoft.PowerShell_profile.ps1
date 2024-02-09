fa724416eee9cb71c5e847c313b1c5592601b97d
Tema <>
2024-02-09 15:14:23 +0100

    Add "Watch-JenkinsLog" cmdlet

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -42,3 +42,35 @@ function psqlps {
 function sqlite3ps {
     sqlite3 --csv --header $args | ConvertFrom-Csv
 }
+
+function Watch-JenkinsLog {
+    param(
+        [Parameter(Mandatory, Position=0)]
+        [string]$Url,
+
+        [Parameter(Mandatory, Position=1)]
+        [string]$Username,
+
+        [Parameter(Mandatory, Position=2)]
+        [string]$TokenPath,
+
+        [Parameter(Position=3)]
+        [float]$Period = 1
+    )
+    $progressive_url = $Url.TrimEnd('/'), 'logText', 'progressiveText' -join "/"
+    $key = Get-Content $TokenPath;
+    $cred = New-Object System.Management.Automation.PSCredential($Username, $(ConvertTo-SecureString $key -AsPlainText));
+    $start = 0;
+    do {
+        $r = (Invoke-WebRequest `
+            -Authentication Basic `
+            -Credential $cred `
+            $progressive_url `
+            -Body @{'start'=$start}
+        );
+        $start = [int]$r.Headers.'X-Text-Size'[0];
+        $done = $null -eq  $r.Headers.'X-More-Data';
+        Write-Host -NoNewline $r.Content;
+        Start-Sleep $Period;
+    } while (!$done)
+}
