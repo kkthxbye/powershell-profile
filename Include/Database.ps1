2dc531a58ba2f9c9069179188f5d4e6ec7e873b5
kkthxbye <kkthxbye@users.noreply.github.com>
2026-08-08 12:09:51 +0200

    Drop legacy psql wrapper

--- Include/Database.ps1
+++ Include/Database.ps1
@@ -3,29 +3,6 @@ function snowsqlps {
     snow sql --silent --format csv $args | Out-String | ConvertFrom-Csv
 }
 
-function psqlps {
-    param (
-        [Parameter(Mandatory, Position = 0)]
-        [string]$ConnectionAlias,
-
-        [Parameter(Mandatory, Position = 1)]
-        [string]$Query,
-
-        [Parameter(ValueFromPipeline)]
-        [string]$InputObject
-    )
-    $tmp = $env:PGSERVICE
-    $env:PGSERVICE = $ConnectionAlias
-    if ($MyInvocation.ExpectingInput) {
-        $input | & psql @args --csv --command "$Query" | Out-String | ConvertFrom-Csv
-    }
-    else {
-        & psql @args --csv --command "$Query" | Out-String | ConvertFrom-Csv
-    }
-
-    $env:PGSERVICE = $tmp
-}
-
 function sqlite3ps {
     sqlite3 --csv --header $args | ConvertFrom-Csv
 }
