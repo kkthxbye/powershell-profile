f6351aa8cbce1af03058250357e41a66382f71ea
kkthxbye <kkthxbye@users.noreply.github.com>
2026-08-09 13:09:27 +0200

    Use cross-platform filepath management

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,18 @@
 $ProfileItem = Get-Item $PROFILE
 $Base = if ($ProfileItem.LinkType) { Split-Path $ProfileItem.Target } else { Split-Path $ProfileItem.FullName }
-$Include = "$Base\Include"
+$Include = Join-Path $Base "Include"
 
-$secrets = . "$Include\Secrets.ps1"
-$configs = . "$Include\Configs.ps1"
+$secrets = . (Join-Path $Include "Secrets.ps1")
+$configs = . (Join-Path $Include "Configs.ps1")
 
-. "$Include\Environment.ps1"
-. "$Include\Aws.ps1"
-. "$Include\Database.ps1"
-. "$Include\Utils.ps1"
+. (Join-Path $Include "Environment.ps1")
+. (Join-Path $Include "Aws.ps1")
+. (Join-Path $Include "Database.ps1")
+. (Join-Path $Include "Utils.ps1")
 
 if ($IsWindows) {
-    . "$Include\Windows.ps1"
+    . (Join-Path $Include "Windows.ps1")
 }
 elseif ($IsLinux) {
-    . "$Include\Linux.ps1"
+    . (Join-Path $Include "Linux.ps1")
 }
