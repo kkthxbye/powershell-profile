550edab0a97666263f52364b40e32e44973ed8bf
Tema <>
2026-08-18 15:58:51 +0200

    Defer loading common modules as they might have dependencies on platform-specific config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -8,8 +8,6 @@ $configsPath = Join-Path $Include "Configs.ps1"
 $secrets = if (Test-Path $secretsPath) { . $secretsPath }
 $configs = if (Test-Path $configsPath) { . $configsPath }
 
-Get-ChildItem (Join-Path $Include "Common") -Filter "*.ps1" | Sort-Object Name | ForEach-Object { . $_.FullName }
-
 if ($IsWindows) {
     . (Join-Path $Include "Windows.ps1")
     Get-ChildItem (Join-Path $Include "Windows") -Filter "*.ps1" | Sort-Object Name | ForEach-Object { . $_.FullName }
@@ -18,3 +16,5 @@ elseif ($IsLinux) {
     . (Join-Path $Include "Linux.ps1")
     Get-ChildItem (Join-Path $Include "Linux") -Filter "*.ps1" | Sort-Object Name | ForEach-Object { . $_.FullName }
 }
+
+Get-ChildItem (Join-Path $Include "Common") -Filter "*.ps1" | Sort-Object Name | ForEach-Object { . $_.FullName }
