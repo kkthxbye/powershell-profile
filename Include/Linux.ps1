b6c37af2bf479483b30a55992fcaaf104d64e0cb
Tema <>
2026-08-18 16:00:27 +0200

    Add homebrew back for the time being :(

--- Include/Linux.ps1
+++ Include/Linux.ps1
@@ -15,6 +15,7 @@ $env:PATH = (@(
         "$env:HOME/.uv/venvs/bin/",
         "$env:HOME/go/bin/",
         "$env:HOME/.duckdb/cli/latest/duckdb",
-        "$env:HOME/.terragrunt/bin"
+        "$env:HOME/.terragrunt/bin",
+        "/home/linuxbrew/.linuxbrew/bin/"
         # $(~/bin/trdl bin-path werf 1.2 stable)
     ) | Join-String -Separator ":"), $env:PATH | Join-String -Separator ":"
