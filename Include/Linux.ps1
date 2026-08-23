cd27db21d1214a1501a52f39bac807802af3b37e
kkthxbye <kkthxbye@users.noreply.github.com>
2026-08-23 12:30:18 +0200

    Fix the ugly indentation on $env:PATH

--- Include/Linux.ps1
+++ Include/Linux.ps1
@@ -1,21 +1,17 @@
-# if ($env:TERM_PROGRAM -eq "vscode") {
-#     . "$(code --locate-shell-integration-path pwsh)"
-# }
-
 $env:SHELL = '/usr/bin/pwsh'
 $env:EDITOR = 'code --wait'
 $env:BROWSER = 'opera.exe'
-$env:PATH = (@(
-        "$env:HOME/.local/bin/",
-        "$env:HOME/bin",
-        "$env:HOME/.cargo/bin",
-        "$env:HOME/.pyenv",
-        "$env:HOME/.pyenv/bin",
-        "$env:HOME/.nvm",
-        "$env:HOME/.uv/venvs/bin/",
-        "$env:HOME/go/bin/",
-        "$env:HOME/.duckdb/cli/latest/duckdb",
-        "$env:HOME/.terragrunt/bin",
-        "/home/linuxbrew/.linuxbrew/bin/"
-        # $(~/bin/trdl bin-path werf 1.2 stable)
-    ) | Join-String -Separator ":"), $env:PATH | Join-String -Separator ":"
+$env:PATH = @(
+    "$env:HOME/.local/bin/",
+    "$env:HOME/bin",
+    "$env:HOME/.cargo/bin",
+    "$env:HOME/.pyenv",
+    "$env:HOME/.pyenv/bin",
+    "$env:HOME/.nvm",
+    "$env:HOME/.uv/venvs/bin/",
+    "$env:HOME/go/bin/",
+    "$env:HOME/.duckdb/cli/latest/duckdb",
+    "$env:HOME/.terragrunt/bin",
+    "/home/linuxbrew/.linuxbrew/bin/"
+    $env:PATH
+) -join ":"
